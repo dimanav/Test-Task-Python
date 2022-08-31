026130ef673258849650fec9a8494b2ee824b096
Дмитрий Гусев <mitai3016@gmail.com>
2022-08-31 10:39:13 +0300

    add fps

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import cv2
 import configparser
+import time
 from PIL import ImageColor
 
 # инициализация
 window_name = "Result"
 face_cascade_db = cv2.CascadeClassifier('faces.xml') # обученая модель с лицами
 bluring = True;
+
+prev_frame_time = 0
+new_frame_time = 0
 
 config = configparser.ConfigParser()  # создаём объекта парсера
 config.read("config.ini")  # читаем конфиг
@@ -21,6 +25,8 @@ source = config["Settings"]["source"]
 if source.isdigit(): source = int(source)
 cap = cv2.VideoCapture(source) 
 
+font = cv2.FONT_HERSHEY_SIMPLEX
+
 
 # обрабатываем изображение
 while True:
@@ -33,6 +39,8 @@ while True:
         print(e)
         
     frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)  # переводим в серый
+    
+    
 
     faces = face_cascade_db.detectMultiScale(frame_gray, 1.1, 5)  # меняем параметры для достижения BEST результата
 
@@ -44,6 +52,17 @@ while True:
     # полный экран
     cv2.namedWindow(window_name, cv2.WND_PROP_FULLSCREEN)
     cv2.setWindowProperty(window_name, cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+    
+    # считаем fps
+    new_frame_time = time.time()
+    fps = 1/(new_frame_time-prev_frame_time)
+    prev_frame_time = new_frame_time
+ 
+    fps = int(fps)
+    fps = str(fps)
+ 
+    # выводим счётчик кадров
+    cv2.putText(frame, f"FPS:{fps}", (7, 60), font, 2, (0, 0, 0), 3, cv2.LINE_AA)
 
     # выводим изображение
     cv2.imshow(window_name, frame)
